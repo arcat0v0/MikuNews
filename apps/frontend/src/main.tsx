0d0db8be145ff50fab027c411b24a6a6aa3127ff
arcat0v0 <i@xvx.rs>
2025-11-26 12:46:38 +0800

    :sparkles: init monorepo projects

---
+++ apps/frontend/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<StrictMode>
+				<App />
+			</StrictMode>
+		),
+	},
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	createRoot(rootElement).render(<RouterProvider router={router} />);
+}
